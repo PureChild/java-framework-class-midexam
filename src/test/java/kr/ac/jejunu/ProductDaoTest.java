4fd90a3e6338691c0b5068a41dcd559d0e846677
purechild <a01056490114@gmail.com>
2018-04-20 15:38:15 +0900

    step08

--- src/test/java/kr/ac/jejunu/ProductDaoTest.java
+++ src/test/java/kr/ac/jejunu/ProductDaoTest.java
@@ -33,9 +33,7 @@ public class ProductDaoTest {
 
     @Test
     public void add() throws SQLException, ClassNotFoundException {
-        Product product = new Product();
-        product.setTitle("상품추가");
-        product.setPrice(1000);
+        Product product = insertProduct();
 
         Long id = productDao.insert(product);
 
@@ -44,5 +42,41 @@ public class ProductDaoTest {
         assertEquals(insertedProduct.getId(), id);
         assertEquals(insertedProduct.getTitle(), product.getTitle());
         assertEquals(insertedProduct.getPrice(), product.getPrice());
+    }
+
+    @Test
+    public void update() throws SQLException {
+        Product product = insertProduct();
+
+        Long id = productDao.insert(product);
+
+        product.setId(id);
+        product.setTitle("상품수정");
+        product.setPrice(5000);
+        productDao.update(product);
+
+        Product updatedProduct = productDao.get(id);
+        assertEquals(updatedProduct.getId(), id);
+        assertEquals(updatedProduct.getTitle(), product.getTitle());
+        assertEquals(updatedProduct.getPrice(), product.getPrice());
+    }
+
+    @Test
+    public void delete() throws SQLException {
+        Product product = insertProduct();
+
+        Long id = productDao.insert(product);
+
+        productDao.delete(id);
+
+        Product deletedProduct = productDao.get(id);
+        assertEquals(deletedProduct, null);
+    }
+
+    private Product insertProduct() {
+        Product product = new Product();
+        product.setTitle("상품추가");
+        product.setPrice(1000);
+        return product;
     }
 }
